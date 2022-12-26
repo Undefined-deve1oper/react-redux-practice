b5e3665652e5b1395d35e6a948b93de4b1400ca5
Undefined-deve1oper <kononovkirill05@gmail.com>
2022-12-26 18:30:00 +0600

    add middleware and compose

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,17 @@
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import taskReducer from "./task";
+import { logger } from "./middleware/logger";
+
+const middlewareEnhancer = applyMiddleware( logger );
 
 function configureStore() {
-    return createStore(taskReducer);
+    return createStore( taskReducer,
+        compose(
+            middlewareEnhancer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    );
 }
 
 export default configureStore;
